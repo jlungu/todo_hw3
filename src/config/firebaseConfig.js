4dac5a5807cd30c53626b4eb3b013ade3cf6fe27
James Lungu <james.lungu@stonybrook.edu>
2019-11-03 15:12:07 -0500

    Fixed database connectivity: REASON - I did not connect the database correctly. Now using my database, and is ready to start development

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,16 +4,16 @@ import 'firebase/auth';
 
 // THIS IS USED TO INITIALIZE THE firebase OBJECT
 // PUT YOUR FIREBASE PROJECT CONFIG STUFF HERE
-var firebaseConfig = {
-    apiKey: "AIzaSyCJxkqx-6PMJrZ7ACkrgbO55b5wmJdop1Y",
-    authDomain: "todo-rrf-316.firebaseapp.com",
-    databaseURL: "https://todo-rrf-316.firebaseio.com",
-    projectId: "todo-rrf-316",
-    storageBucket: "todo-rrf-316.appspot.com",
-    messagingSenderId: "892398996038",
-    appId: "1:892398996038:web:1fb9157fc6c5d266e01847",
-    measurementId: "G-TEGQB3MZ23"
-};
+var firebaseConfig = {
+        apiKey: "AIzaSyD2ARzycv5567O6oxhb27NCKfm1h_Vt30c",
+        authDomain: "jlungu-todo-hw3-921af.firebaseapp.com",
+        databaseURL: "https://jlungu-todo-hw3-921af.firebaseio.com",
+        projectId: "jlungu-todo-hw3-921af",
+        storageBucket: "jlungu-todo-hw3-921af.appspot.com",
+        messagingSenderId: "538651759915",
+        appId: "1:538651759915:web:9d089329c04a18755d216e",
+        measurementId: "G-X2DKWRKXCL"
+      };
 firebase.initializeApp(firebaseConfig);
 
 // NOW THE firebase OBJECT CAN BE CONNECTED TO THE STORE
