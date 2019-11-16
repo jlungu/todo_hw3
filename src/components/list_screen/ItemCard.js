ecfc94cf23e90d8bc0af2d8a1db578e88cc9c301
James Lungu <james.lungu@stonybrook.edu>
2019-11-16 16:34:50 -0500

    Added display of various item card information such as due date, completed status, assigned to status, etc

--- src/components/list_screen/ItemCard.js
+++ src/components/list_screen/ItemCard.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 class ItemCard extends React.Component {
-    render() {
-        const { item, todoList } = this.props;
-        return (
-            <Link to={"/todoList/" + todoList.id + "/items/" + item.key}>
-                <div className="card z-depth-0 todo-list-link pink-lighten-3"></div>
-            <div className="card z-depth-0 todo-list-link pink-lighten-3">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">{item.description}</span>
-                    <span className="black-text">Assigned To: {item.assigned_to}</span>
-                </div>
-                <div className="card z-depth-0 todo-list-link pink-lighten-3">
-                <span className="card-title">{item.due_date}</span>
-                </div>
+  render() {
+    const { item, todoList } = this.props;
+    return (
+      <Link to={"/todoList/" + todoList.id + "/items/" + item.key}>
+        <div className="card z-depth-0 todo-list-link pink-lighten-3">
+          <div class="row">
+            <div class="col s3">
+              <div className="card-content grey-text text-darken-3">
+                <span className="card-title">{item.description}</span>
+                <span className="black-text">
+                  Assigned To: {item.assigned_to}
+                </span>
+              </div>
             </div>
-            </Link>
-        );
-    }
+            <div class="col s3" id="item_card_due_date">{item.due_date}</div>
+            <div class="col s3" id="item_card_completed">{item.completed? "Completed": "Incompleted"}</div>
+            <div class="col s3">
+                
+            </div>
+          </div>
+        </div>
+      </Link>
+    );
+  }
 }
 export default ItemCard;
